6136c78adfdcf3f7d476c935aaf123606204903c
Jeremy Rogers <info@jero.co.nz>
2020-10-06 15:15:38 +1300

    Support for rural delivery detection and different pricing

--- elements/shipping_method_types/nz_flat_rate/dashboard_form.php
+++ elements/shipping_method_types/nz_flat_rate/dashboard_form.php
@@ -1,21 +1,35 @@
 <?php
-defined('C5_EXECUTE') or die("Access Denied.");
+defined('C5_EXECUTE') or die('Access Denied.');
 extract($vars); ?>
 <div class="row">
 	<div class="col-xs-12 col-sm-6">
-		<?= $form->label('north',t("North Island")); ?>
+		<?= $form->label('north',t('North Island')); ?>
 		<div class="input-group">
 			<div class="input-group-addon"><?=Config::get('community_store.symbol')?></div>
 			<?= $form->text('north',$smtm->getNorth()); ?>
 		</div>
 	</div>
+    <div class="col-xs-12 col-sm-6">
+		<?= $form->label('northRD',t('North Island RD')); ?>
+        <div class="input-group">
+            <div class="input-group-addon"><?=Config::get('community_store.symbol')?></div>
+			<?= $form->text('northRD',$smtm->getNorthRD()); ?>
+        </div>
+    </div>
 </div>
 <div class="row">
 	<div class="col-xs-12 col-sm-6">
-		<?= $form->label('south',t("South Island")); ?>
+		<?= $form->label('south',t('South Island')); ?>
 		<div class="input-group">
 			<div class="input-group-addon"><?=Config::get('community_store.symbol')?></div>
 			<?= $form->text('south',$smtm->getSouth()); ?>
 		</div>
 	</div>
+    <div class="col-xs-12 col-sm-6">
+		<?= $form->label('southRD',t('South Island RD')); ?>
+        <div class="input-group">
+            <div class="input-group-addon"><?=Config::get('community_store.symbol')?></div>
+			<?= $form->text('southRD',$smtm->getSouthRD()); ?>
+        </div>
+    </div>
 </div>
